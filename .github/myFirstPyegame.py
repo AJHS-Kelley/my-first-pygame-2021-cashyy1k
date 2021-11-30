282395a32cebef80071e2dccfcc6af750574356a
Anthony Furlow <90413314+cashyy1k@users.noreply.github.com>
2021-11-30 14:36:01 -0500

    v0.4
    added color values

--- .github/myFirstPyegame.py
+++ .github/myFirstPyegame.py
@@ -1,4 +1,20 @@
-# My First PyGame, Anthony Furlow, 11/30/21, 2:19 pm, v0.1
+# My First PyGame, Anthony Furlow, 11/30/21, 2:35 pm, v0.1
 
 import pygame, sys
 from pygame.locals import *
+
+# Start Pygame
+pygame.init()
+
+#Setup the game window
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello, world!')
+
+# Step color vaules.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+JAVO = (0, 255, 255)
+
