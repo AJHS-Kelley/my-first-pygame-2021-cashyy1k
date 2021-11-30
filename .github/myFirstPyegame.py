5244bf2c924deafaa1fc83db3c3e524f1604cc13
Anthony Furlow <90413314+cashyy1k@users.noreply.github.com>
2021-11-30 14:51:03 -0500

    v0.6
    drew background color

--- .github/myFirstPyegame.py
+++ .github/myFirstPyegame.py
@@ -26,3 +26,6 @@ text = basicFont.render('Hello, world!', True, WHITE, BLUE)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centerx = windowSurface.get_rect().centery
+
+# Draw the game background
+windowSurface.fill(JAVO)
