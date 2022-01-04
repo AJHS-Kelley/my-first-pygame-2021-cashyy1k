d36e40323a39cb741d75edaccd34ead706f2013a
Anthony Furlow <furlow3.annie@gmail.com>
2022-01-04 14:48:42 -0500

    v0.1

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,1 +1,13 @@
 # Simple Animation with PyGame, Anthony Furlow, 1/4/22, 2:34 pm, v0.0
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+
+# Setup the Window
+WINDOWWITH = 400
+WINDOWHEIGHT = 400
+windowSurface = = pygame.display.set_mode((WINDOWWITH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
