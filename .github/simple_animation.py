ab99e3a3a1d0872c4d6ad38d3dcd58aa51f671fb
Anthony Furlow <furlow3.annie@gmail.com>
2022-01-12 13:36:54 -0500

    v0.4
    created the box values

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,5 +1,6 @@
 # Simple Animation with PyGame, Anthony Furlow, 1/4/22, 2:34 pm, v0.0
 
+from PyGamePractice import GREEN
 import pygame, sys, time
 from pygame.locals import *
 
@@ -25,3 +26,9 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Setup the box data.
+b1 = {'react':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
+b2 = {'react':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'react':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+boxes = [b1, b2, b3]
