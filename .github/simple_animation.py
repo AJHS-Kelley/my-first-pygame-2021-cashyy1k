25598de0cb09c705960ff9973378d6827a7b614d
Anthony Furlow <furlow3.annie@gmail.com>
2022-01-12 13:51:48 -0500

    v0.5
    setup the main game loop.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -32,3 +32,11 @@ b1 = {'react':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
 b2 = {'react':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
 b3 = {'react':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
 boxes = [b1, b2, b3]
+
+# Run the game loop.
+while True:
+    # Check for QUIT event.
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
